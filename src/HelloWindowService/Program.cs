8434fb0c6fbe8c60671420e4b859f4824c201cae
unknown <xiaoxm_001@163.com>
2016-06-18 16:13:17 +0800

    import projects

---
+++ src/HelloWindowService/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace HelloWindowService
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            TestCode();
+            Console.Read();
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+
+        }
+
+        private static void TestCode()
+        {
+            string path = @"c:\good.txt";
+            if (!File.Exists(path))
+                File.Create(path);
+            File.AppendAllText(path, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "hello windows service Debug" + "\r\n");
+        }
+    }
+}
---
+++ src/HelloWindowService/Service1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace HelloWindowService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            TestCode();
+        }
+
+        private static void TestCode()
+        {
+            string path = @"c:\good.txt";
+            if (!File.Exists(path))
+                File.Create(path);
+            File.AppendAllText(path, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "hello windows service Release"+"\r\n");
+        }
+
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
